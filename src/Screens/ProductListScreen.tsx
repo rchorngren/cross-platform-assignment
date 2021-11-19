feeeadfa21290458dd953eb1f6babcb68a7a95fd
Robert Horngren <robert@horngren.net>
2021-11-19 12:33:06 +0100

    added mockup view of ProductListScreen

--- src/Screens/ProductListScreen.tsx
+++ src/Screens/ProductListScreen.tsx
@@ -1,28 +1,81 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, Pressable } from "react-native";
 import { StackScreen } from "../helpers/types";
 
-interface IProductListScreen extends NativeStackScreenProps<StackScreen, 'ProductListScreen'> { }
+interface IProductListScreen extends NativeStackScreenProps<StackScreen, "ProductListScreen"> { }
 
 export const ProductListScreen: React.FC<IProductListScreen> = (props) => {
+  const [savedProducts, setSavedProducts] = useState(null);
 
   return (
     <View style={styles.container}>
-      <Text>Hello from ProductListScreen</Text>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Items</Text>
+      </View>
+
+      <View style={styles.subHeader}>
+        <Text style={styles.subHeaderText}>Name</Text>
+        <Text style={styles.subHeaderText}>Type</Text>
+        <Text style={styles.subHeaderText}>Price</Text>
+      </View>
+
+      <View style={styles.content}>
+        {savedProducts ? (
+          null
+        ) : (
+          <Text style={styles.noItemText}>You do not have any products.{"\n"}Press the green button below to add a new one</Text>
+        )}
+
+      </View>
+
       <Pressable
         onPress={() => props.navigation.navigate("AddProductScreen")}
       >
         <Text>Add product</Text>
       </Pressable>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  header: {
+    height: 50,
+    justifyContent: "center",
+    backgroundColor: "green"
+  },
+  headerText: {
+    marginLeft: 15,
+    fontSize: 25,
+    color: "white"
+  },
+  subHeader: {
+    height: 30,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: "black",
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  subHeaderText: {
+    fontWeight: "bold"
+  },
+  content: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center"
+  },
+  noItemText: {
+    textAlign: "center",
+    fontSize: 16,
+    marginLeft: 15,
+    marginRight: 15
   }
 })
